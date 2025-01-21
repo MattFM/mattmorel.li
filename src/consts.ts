8ec49a20b860b9a6b32512ebe4223e2975faa25f
Matt Morelli <mmorelli@gmail.com>
2025-01-21 13:48:56 +0000

    Initial Commit following fork

--- src/consts.ts
+++ src/consts.ts
@@ -1,6 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Devolio';
-export const SITE_TAGLINE = 'Minimalist Starter for Developer Blog & Portfolio Website'; // site tagline
-export const SITE_DESCRIPTION = 'Minimalist Starter for Developer Blog & Portfolio Website'; // site meta description for SEO
+export const SITE_TITLE = 'Matt Morelli';
+export const SITE_TAGLINE = 'Newbie Frontend Developer'; // site tagline
+export const SITE_DESCRIPTION = 'Blog and projects of Matt Morelli, Inverness-based frontend developer'; // site meta description for SEO
